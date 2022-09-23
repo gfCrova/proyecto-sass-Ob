66262a9dae4a3f3afcfb3911869ca57c8652b3a8
Gian Franco Crova <103906625+gfCrova@users.noreply.github.com>
2022-09-23 00:35:11 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,18 @@
+
+
+const user = {
+    nombre: "javier",
+    edad: 20,
+    comidas: ["asado", "verduras", "postre"],
+    auto: {
+        marca: "ford",
+        color: "negro",
+        velocidadMax: 200,
+        partes: {
+            ruedas: 4,
+            puertas: 5,
+            asientos: 4,
+            marchas: [1, 2, 3, 4, 5, 6]
+        }
+    }
+}
